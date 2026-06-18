256899f42e2017daccebd85df376f25393e09174
Tim <github@timgreen.ws>
2026-06-18 21:18:49 +1000

    added astro-minify-html-swc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import minify from 'astro-minify-html-swc'
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()]
+  integrations: [react(), minify()]
 });
